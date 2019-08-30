277e55d5aef3e220661f4184a0b70d7b983aef55
JiangWeixian <saber_wei@163.com>
2019-08-30 11:44:41 +0800

    chore: init examples basic-react

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import ms from 'ms'
-import lunchtime from './lunchtime.js'
-import millisecondsUntil from './millisecondsUntil.js'
-
-export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
-  const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes))
-  return ms(millisecondsUntilLunchTime, { long: true })
+const loader = (source: string) => {
+  return source
 }
+
+export default loader
