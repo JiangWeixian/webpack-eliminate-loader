1b225c25a9728f3575a24fa969fb38fbcf6031fa
JiangWeixian <saber_wei@163.com>
2019-09-09 15:26:44 +0800

    chore: add options validate

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,28 +1,36 @@
 import Webpack from 'webpack'
 import loadUtils from 'loader-utils'
-// import validate from 'schema-utils'
+import schemaValidate from 'schema-utils'
+import { validate } from './validate'
 import { Options } from '../typings'
 
-// const schema = {
-//   type: 'object',
-//   properties: {
-//     exclude: {
-//       type: 'array',
-//       description: 'exclude these files in webpack build',
-//     },
-//     include: {
-//       type: 'array',
-//       description: 'include these files in webpack build',
-//     },
-//     presets: {
-//       type: 'string',
-//       description: 'the way of how this webpack work',
-//     },
-//   },
-// }
+const schema = {
+  type: 'object',
+  properties: {
+    exclude: {
+      type: 'array',
+      description: 'exclude these files in webpack build',
+    },
+    include: {
+      type: 'array',
+      description: 'include these files in webpack build',
+    },
+    presets: {
+      type: 'array',
+      description: 'the way of how this webpack work',
+    },
+  },
+}
 
-export const validateOptions = () => {}
+export const validateOptions = (options: Options) => {
+  schemaValidate(schema, options, 'webpack-eliminate-loader')
+  validate([
+    [!!(options.include && options.exclude), 'options.include and options.exclude is conflict'],
+  ])
+}
 
 export const getOptions = (context: Webpack.loader.LoaderContext): Options => {
-  return loadUtils.getOptions(context)
+  const options = loadUtils.getOptions(context)
+  validateOptions(options)
+  return options
 }
